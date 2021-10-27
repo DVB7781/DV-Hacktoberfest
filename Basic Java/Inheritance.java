e280dd8cda14b912893a2ec766269b6b5b28c5d2
Birendrakumarmahto <60342800+Birendrakumarmahto@users.noreply.github.com>
2021-10-27 16:51:13 +0530

    Update Inheritance.java

--- Basic Java/Inheritance.java
+++ Basic Java/Inheritance.java
@@ -1,28 +1,72 @@
-class Animal{
-	void walk(){
-		System.out.println("I am walking");
+// Java program to illustrate the
+// concept of inheritance
+
+// base class
+class Bicycle {
+	// the Bicycle class has two fields
+	public int gear;
+	public int speed;
+
+	// the Bicycle class has one constructor
+	public Bicycle(int gear, int speed)
+	{
+		this.gear = gear;
+		this.speed = speed;
+	}
+
+	// the Bicycle class has three methods
+	public void applyBrake(int decrement)
+	{
+		speed -= decrement;
+	}
+
+	public void speedUp(int increment)
+	{
+		speed += increment;
+	}
+
+	// toString() method to print info of Bicycle
+	public String toString()
+	{
+		return ("No of gears are " + gear + "\n"
+				+ "speed of bicycle is " + speed);
 	}
 }
 
+// derived class
+class MountainBike extends Bicycle {
 
-class Bird extends Animal{
-	void fly(){
-		System.out.println("I am flying");
-	}
-	//code need to be added
-    void sing(){
-        System.out.println("I am singing");
-    }
-}
+	// the MountainBike subclass adds one more field
+	public int seatHeight;
 
-
-public class Inheritance {
-	public static void main(String args[]){
-
-		  Bird bird = new Bird();
-		  bird.walk();
-		  bird.fly();
-	      bird.sing();
+	// the MountainBike subclass has one constructor
+	public MountainBike(int gear, int speed,int startHeight)
+	{
 		
-	   }
+		super(gear, speed);
+		seatHeight = startHeight;
 	}
+
+	
+	public void setHeight(int newValue)
+	{
+		seatHeight = newValue;
+	}
+
+	
+	@Override 
+	public String toString()
+	{
+		return (super.toString() + "\nseat height is "+ seatHeight);
+	}
+}
+
+// driver class
+public class Test {
+	public static void main(String args[])
+	{
+
+		MountainBike mb = new MountainBike(3, 100, 25);
+		System.out.println(mb.toString());
+	}
+}
